7dbf6f7d2813f4e5a5c093c6909779e454675d47
Fefeprogrammer <50031002+Fefeprogrammer@users.noreply.github.com>
2020-10-12 06:10:37 -0300

    Pirmeiro Comit

--- Game_winform/Form1.Designer.cs
+++ Game_winform/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Game_winform
 {
-    partial class Form1
+    partial class Enemy
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -29,12 +29,83 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.Ground = new System.Windows.Forms.PictureBox();
+            this.Inimigo = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Inimigo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ground
+            // 
+            this.Ground.Image = global::Game_winform.Properties.Resources.Ground;
+            this.Ground.Location = new System.Drawing.Point(-4, 425);
+            this.Ground.Name = "Ground";
+            this.Ground.Size = new System.Drawing.Size(803, 47);
+            this.Ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ground.TabIndex = 2;
+            this.Ground.TabStop = false;
+            // 
+            // Inimigo
+            // 
+            this.Inimigo.Image = global::Game_winform.Properties.Resources.Enemy;
+            this.Inimigo.Location = new System.Drawing.Point(114, 55);
+            this.Inimigo.Name = "Inimigo";
+            this.Inimigo.Size = new System.Drawing.Size(100, 50);
+            this.Inimigo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Inimigo.TabIndex = 1;
+            this.Inimigo.TabStop = false;
+            // 
+            // Player
+            // 
+            this.Player.Image = global::Game_winform.Properties.Resources.Player;
+            this.Player.Location = new System.Drawing.Point(331, 294);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(129, 93);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            this.Player.Click += new System.EventHandler(this.Player_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 3000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Enemy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 475);
+            this.Controls.Add(this.Ground);
+            this.Controls.Add(this.Inimigo);
+            this.Controls.Add(this.Player);
+            this.Name = "Enemy";
+            this.Text = "Game";
+            this.Load += new System.EventHandler(this.Enemy_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Inimigo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox Inimigo;
+        private System.Windows.Forms.PictureBox Ground;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- Game_winform/Form1.cs
+++ Game_winform/Form1.cs
@@ -10,11 +10,79 @@ using System.Windows.Forms;
 
 namespace Game_winform
 {
-    public partial class Form1 : Form
+    public partial class Enemy : Form
     {
-        public Form1()
+        public int speed = 15;
+
+        public Enemy()
         {
             InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                Player.Location = new Point(Player.Location.X, Player.Location.Y - speed);
+            }
+
+            if(e.KeyCode == Keys.Down)
+            {
+                Player.Location = new Point(Player.Location.X, Player.Location.Y + speed);
+            }
+
+            if(e.KeyCode == Keys.Right)
+            {
+                Player.Location = new Point(Player.Location.X + speed, Player.Location.Y);
+            }
+
+            if(e.KeyCode == Keys.Left)
+            {
+                Player.Location = new Point(Player.Location.X - speed, Player.Location.Y);
+            }
+
+            if(e.KeyCode == Keys.W)
+            {
+               
+                Player.Location = new Point(Player.Location.X, Player.Location.Y - 100);
+                Player.Location = new Point(Player.Location.X, Player.Location.Y + 100);
+            }
+
+        }
+
+        private void tb_velocidade_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random randX = new Random();
+            Random randY = new Random();
+
+            int valorY = randY.Next(100, 500);
+            int valorX = randX.Next(100, 500);
+
+            //Inimigo.Location = new Point(Inimigo.Location.X + valorX, Inimigo.Location.Y + valorY);
+            Inimigo.Visible = true;
+            timer2.Enabled = true;
+        }
+
+        private void Enemy_Load(object sender, EventArgs e)
+        {
+            Inimigo.Visible = false;
+            timer1.Enabled = true;
+        }
+
+        private void Player_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Inimigo.Visible = false;
+            timer1.Enabled = true;
         }
     }
 }
